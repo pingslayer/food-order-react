82b84d045b097f07dfb5ca98616a837aa8347815
pingslayer <ronaldprajval7@gmail.com>
2021-08-10 11:29:33 +0530

    Adding a Modal via a React Portal

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,3 +1,5 @@
+//components
+import Modal from "../UI/Modal";
 //css
 import classes from "./Cart.module.css";
 
@@ -11,7 +13,7 @@ const Cart = (props) => {
   );
 
   return (
-    <div>
+    <Modal>
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
@@ -21,7 +23,7 @@ const Cart = (props) => {
         <button className={classes["button--alt"]}>Close</button>
         <button className={classes.button}>Order</button>
       </div>
-    </div>
+    </Modal>
   );
 };
 
---
+++ src/components/UI/Modal.js
@@ -0,0 +1,32 @@
+import { Fragment } from "react";
+import ReactDOM from "react-dom";
+//css
+import classes from "./Modal.module.css";
+
+const portalElement = document.getElementById("overlays");
+
+const Backdrop = (props) => {
+  return <div className={classes.backdrop}></div>;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}>{props.children}</div>
+    </div>
+  );
+};
+
+const Modal = (props) => {
+  return (
+    <Fragment>
+      {ReactDOM.createPortal(<Backdrop />, portalElement)}
+      {ReactDOM.createPortal(
+        <ModalOverlay>{props.children}</ModalOverlay>,
+        portalElement
+      )}
+    </Fragment>
+  );
+};
+
+export default Modal;
